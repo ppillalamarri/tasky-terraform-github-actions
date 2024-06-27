ba38a3b97d9ef3f64ea5e63e513ae61019f71748
ppillalamarri <ppillalamarri@gmail.com>
2024-06-27 23:25:10 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
+#Create an EKS cluster instance in the same VPC as your database server
+#Build and host a container image for your web application*
+#Ensure your built container image contains an arbitrary file called “wizexercise.txt” with some content
+#Deploy your container-based web application to the EKS cluster
+# Configure your EKS cluster to grant cluster-admin privileges to your web application container(s)
+
+#TODO Ensure your web application authenticates to your database server (connection strings are a common approach)
+# TODO Allow public internet traffic to your web application using service type loadbalancer
 
