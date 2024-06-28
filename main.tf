29fc5333198d6a774aa580b851ea51ce82d3b09f
ppillalamarri <ppillalamarri@gmail.com>
2024-06-28 09:00:10 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,3 +52,34 @@ resource "aws_subnet" "eks_subnet_b" {
     Name = "eks_subnet_b"
   }
 }
+
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+  tags = {
+    Name = "eks_igw"
+  }
+}
+
+resource "aws_route_table" "eks_route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_igw.id
+  }
+
+  tags = {
+    Name = "eks_route_table"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.eks_subnet_a.id
+  route_table_id = aws_route_table.eks_route_table.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.eks_subnet_b.id
+  route_table_id = aws_route_table.eks_route_table.id
+}
+
