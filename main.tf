c9b532754d231e895617bd0af7821449c3dfc0f0
ppillalamarri <ppillalamarri@gmail.com>
2024-06-28 19:37:37 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -177,6 +177,25 @@ resource "aws_ecr_lifecycle_policy" "default_policy" {
 	EOF
 }
 
+
+# Checks if build folder has changed
+data "external" "build_dir" {
+  program = ["bash", "${path.module}/bin/dir_md5.sh", var.dockerfile_dir]
+}
+
+# Builds test-service and pushes it into aws_ecr_repository
+resource "null_resource" "ecr_image" {
+  triggers = {
+    build_folder_content_md5 = data.external.build_dir.result.md5
+  }
+
+  # Runs the build.sh script which builds the dockerfile and pushes to ecr
+  provisioner "local-exec" {
+    command = "bash ${path.module}/bin/build.sh ${var.dockerfile_dir} ${var.ecr_repository_url}:${var.docker_image_tag}"
+  }
+}
+
+
 resource "aws_iam_role" "eks_role" {
   name               = "eks-cluster-role"
   assume_role_policy = data.aws_iam_policy_document.eks_assume_role_policy.json
