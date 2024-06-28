376fa2c31bbac058ae6c9fd42dc3dec0e2793253
ppillalamarri <ppillalamarri@gmail.com>
2024-06-28 15:36:27 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -184,7 +184,8 @@ resource "null_resource" "docker_packaging" {
 	
 	  provisioner "local-exec" {
 	    command = <<EOF
-	    aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin "070009232298.dkr.ecr.eu-west-1.amazonaws.com"
+	    sudo yum install docker
+            aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin "070009232298.dkr.ecr.eu-west-1.amazonaws.com"
 	    docker build -t "${aws_ecr_repository.app_ecr_repo.repository_url}:latest" -f ./Dockerfile .
 	    docker push "${aws_ecr_repository.app_ecr_repo.repository_url}:latest"
 	    EOF
