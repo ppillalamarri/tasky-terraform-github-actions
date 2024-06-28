ae6fc47a527a7a50811c4ece04d6b1d7e630cdbf
ppillalamarri <ppillalamarri@gmail.com>
2024-06-28 03:22:15 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,121 +31,80 @@ provider "aws" {
   secret_key = "uuIl8NxNJAFVu7/VXLYKH0zmhrFXoRn9APXB8I6r"
 }
 
+data "aws_availability_zones" "available" {}
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
 }
 
-resource "aws_subnet" "eks_subnet_a" {
-  vpc_id     = aws_vpc.eks_vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-west-1a"
-  tags = {
-    Name = "eks_subnet_a"
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+locals {
+  cluster_name = "learnk8s"
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+module "eks-kubeconfig" {
+  source     = "hyperbadger/eks-kubeconfig/aws"
+  version    = "1.0.0"
+
+  depends_on = [module.eks]
+  cluster_id =  module.eks.cluster_id
+  }
+
+resource "local_file" "kubeconfig" {
+  content  = module.eks-kubeconfig.kubeconfig
+  filename = "kubeconfig_${local.cluster_name}"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.18.1"
+
+  name                 = "k8s-vpc"
+  cidr                 = "172.16.0.0/16"
+  azs                  = data.aws_availability_zones.available.names
+  private_subnets      = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
+  public_subnets       = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
   }
 }
 
-resource "aws_subnet" "eks_subnet_b" {
-  vpc_id     = aws_vpc.eks_vpc.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-west-1b"
-  tags = {
-    Name = "eks_subnet_b"
-  }
-}
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "18.30.3"
 
-resource "aws_internet_gateway" "eks_igw" {
-  vpc_id = aws_vpc.eks_vpc.id
-  tags = {
-    Name = "eks_igw"
-  }
-}
+  cluster_name    = "${local.cluster_name}"
+  cluster_version = "1.24"
+  subnet_ids      = module.vpc.private_subnets
 
-resource "aws_route_table" "eks_route_table" {
-  vpc_id = aws_vpc.eks_vpc.id
+  vpc_id = module.vpc.vpc_id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.eks_igw.id
-  }
-
-  tags = {
-    Name = "eks_route_table"
-  }
-}
-
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.eks_subnet_a.id
-  route_table_id = aws_route_table.eks_route_table.id
-}
-
-resource "aws_route_table_association" "b" {
-  subnet_id      = aws_subnet.eks_subnet_b.id
-  route_table_id = aws_route_table.eks_route_table.id
-}
-
-resource "eks" "wizdemoeks" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "my-eks-cluster"
-  cluster_version = "1.21"
-  subnets         = [aws_subnet.eks_subnet_a.id, 
-                        aws_subnet.eks_subnet_b.id]
-  vpc_id          = aws_vpc.eks_vpc.id
-
-  node_groups = {
-    eks_nodes = {
-      desired_capacity = 2
-      max_capacity     = 3
+  eks_managed_node_groups = {
+    first = {
+      desired_capacity = 1
+      max_capacity     = 10
       min_capacity     = 1
 
-      instance_type = "t2.micro"
-
-      key_name = "existing_wizdemokeypair"  # Ensure you have a valid EC2 key pair
-
-      additional_tags = {
-        Name = "eks_nodes"
-      }
+      instance_type = "m5.large"
     }
   }
-}
-
-data "aws_iam_policy_document" "eks_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role" "eks_role" {
-  name               = "eks-cluster-role"
-  assume_role_policy = data.aws_iam_policy_document.eks_assume_role_policy.json
-}
-
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "eks_service_policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = aws_iam_role.eks_role.name
-}
-
-output "cluster_id" {
-  description = "EKS cluster ID"
-  value       = module.eks.cluster_id
-}
-
-output "cluster_endpoint" {
-  description = "EKS cluster endpoint"
-  value       = module.eks.cluster_endpoint
-}
-
-output "cluster_security_group_id" {
-  description = "EKS cluster security group ID"
-  value       = module.eks.cluster_security_group_id
 }
