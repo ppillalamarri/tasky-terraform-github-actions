6262a17109b120191442f168d13b3ed814c3bb44
ppillalamarri <ppillalamarri@gmail.com>
2024-06-28 11:47:05 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.40.0"
+      version < "5.0"
     }
     random = {
       source  = "hashicorp/random"
