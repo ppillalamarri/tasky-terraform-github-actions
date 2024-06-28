b2e150040bbc7da138eb4a1fb084b843f0681592
ppillalamarri <ppillalamarri@gmail.com>
2024-06-28 20:10:00 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,13 @@
 variable "dockerfile_dir" {
   type = string
   description = "The directory that contains the Dockerfile"
+  default = "."
 }
 
 variable "ecr_repository_url" {
   type        = string
   description = "Full url for the ECR repository"
+  default = "https://070009232298.dkr.ecr.eu-west-1.amazonaws.com/app-repo"
 }
 
 variable "docker_image_tag" {
