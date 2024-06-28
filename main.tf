b08bedcddd0d4ce4dae235a1bab0a3cc54ba1326
ppillalamarri <ppillalamarri@gmail.com>
2024-06-28 12:15:30 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.0"
+      version = "~> 4.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -31,83 +31,91 @@ provider "aws" {
   secret_key = "uuIl8NxNJAFVu7/VXLYKH0zmhrFXoRn9APXB8I6r"
 }
 
-data "aws_availability_zones" "available" {}
-
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
-
-locals {
-  cluster_name = "wizdemok8s"
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-module "eks-kubeconfig" {
-  source     = "hyperbadger/eks-kubeconfig/aws"
-  version    = "1.0.0"
-
-  depends_on = [module.eks]
-  cluster_id =  module.eks.cluster_id
-  }
-
-resource "local_file" "kubeconfig" {
-  content  = module.eks-kubeconfig.kubeconfig
-  filename = "kubeconfig_${local.cluster_name}"
+data "aws_availability_zones" "available" {
+  state = "available"
 }
 
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.18.1"
+  source = "terraform-aws-modules/vpc/aws"
+  version = "4.0.0"
 
-  name                 = "k8s-vpc"
-  cidr                 = "172.16.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
-  private_subnets      = []
-  public_subnets       = ["172.31.0.0/20", "172.31.16.0/20", "172.31.32.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
+  name = "stw-vpc"
+  cidr = "10.0.0.0/16"
 
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = "1"
-  }
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = ["10.0.0.0/22", "10.0.4.0/22", "10.0.8.0/22"]
+  public_subnets  = ["10.0.100.0/22", "10.0.104.0/22", "10.0.108.0/22"]
 
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = "1"
-  }
+  enable_nat_gateway = true
+  single_nat_gateway = true
+  enable_vpn_gateway = false
+
 }
+
+
+locals {
+  vpc_id              = module.vpc.vpc_id
+  vpc_cidr            = module.vpc.vpc_cidr_block
+  public_subnets_ids  = module.vpc.public_subnets
+  private_subnets_ids = module.vpc.private_subnets
+  subnets_ids         = concat(local.public_subnets_ids, local.private_subnets_ids)
+}
+
+
+
+################
+#  EKS MODULE  #
+################
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "18.30.3"
+  version = "~> 19.0"
 
-  cluster_name    = "${local.cluster_name}"
+  cluster_name    = "stw-cluster"
   cluster_version = "1.24"
-  subnet_ids      = module.vpc.private_subnets
 
-  vpc_id = module.vpc.vpc_id
+  cluster_endpoint_public_access = true
 
-  eks_managed_node_groups = {
-    first = {
-      desired_capacity = 1
-      max_capacity     = 3
-      min_capacity     = 1
-
-      instance_type = "t2.micro"
+  cluster_addons = {
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent              = true
+      before_compute           = true
+      service_account_role_arn = module.vpc_cni_irsa.iam_role_arn
+      configuration_values = jsonencode({
+        env = {
+          # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
+          ENABLE_PREFIX_DELEGATION = "true"
+          WARM_PREFIX_TARGET       = "1"
+        }
+      })
     }
   }
+
+  vpc_id                   = local.vpc_id
+  subnet_ids               = local.private_subnets_ids
+  control_plane_subnet_ids = local.private_subnets_ids
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    ami_type                   = "AL2_x86_64"
+    instance_types             = ["t2.micro"]
+    iam_role_attach_cni_policy = true
+  }
+
+  eks_managed_node_groups = {
+    stw_node_wg = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+  }
+
 }
+
+
 
 data "aws_iam_policy_document" "eks_assume_role_policy" {
   statement {
@@ -116,6 +124,26 @@ data "aws_iam_policy_document" "eks_assume_role_policy" {
     principals {
       type        = "Service"
       identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
+
+################################
+#  ROLES FOR SERVICE ACCOUNTS  #
+################################
+
+module "vpc_cni_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.0"
+
+  role_name_prefix      = "VPC-CNI-IRSA"
+  attach_vpc_cni_policy = true
+  vpc_cni_enable_ipv4   = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-node"]
     }
   }
 }
