157d70e91ddd9d3f55429e6e8b794f58022132d4
ppillalamarri <ppillalamarri@gmail.com>
2024-06-30 16:36:34 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -117,6 +117,11 @@ output "eks_cluster_ca_certificate" {
 provider "kubernetes" {
   host                   = aws_eks_cluster.example.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.example.certificate_authority.0.data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args = ["eks", "get-token", "--cluster-name", aws_eks_cluster.example.name]
+    command = "aws"
+  }
   token                  = data.aws_eks_cluster_auth.example.token
 }
 
