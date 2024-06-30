12a2d71b8814790d6a9c0f15c688a74115b061f1
ppillalamarri <ppillalamarri@gmail.com>
2024-06-30 13:50:50 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,20 +15,20 @@ resource "aws_eks_cluster" "example" {
 
 
 # IAM Role for EKS Cluster
-resource "aws_iam_role" "eks_cluster" {
-  name = "eks-cluster-role"
+#resource "aws_iam_role" "eks_cluster" {
+#  name = "eks-cluster-role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-    }]
-  })
-}
+#  assume_role_policy = jsonencode({
+#    Version = "2012-10-17"
+#    Statement = [{
+#      Action = "sts:AssumeRole"
+#      Effect = "Allow"
+#      Principal = {
+#        Service = "eks.amazonaws.com"
+#      }
+#    }]
+#  })
+#}
 
 # Attach Policies to EKS Cluster Role
 resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
