4396fab5883208c8b5eb2e86fe1a085a8a3bc929
ppillalamarri <ppillalamarri@gmail.com>
2024-07-01 01:14:03 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -63,8 +63,8 @@ resource "aws_security_group" "mdbport" {
   }
 
   egress {
-    from_port   = 27017
-    to_port     = 27017
+    from_port   = 0
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
